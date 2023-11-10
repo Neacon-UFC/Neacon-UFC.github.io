9625f70435f3f570168bcada12ec6032af20dca7
Joel Kalil <joelkalil1@gmail.com>
2023-11-10 15:49:20 -0300

    testing

--- src/js/headerFooter.js
+++ src/js/headerFooter.js
@@ -1,53 +1,26 @@
 // Global Variables
-var language ={
-    "header":{
-        "home": "Home",
-
-        "research": "Research",
-        "robotics": "Robotics",
-        "control_theory": "Control Theory",
-        "motor_driven_system_control": "Motor Driven System Control",
-        "data_based_control": "Data Based Control",
-
-        "publications": "Publications",
-        "projects": "Projects",
-        "project_1": "Project 1",
-        "project_2": "Project 2",
-        "project_3": "Project 3",
-
-        "team": "Team",
-        "contact": "Contact",
-
-        "search": "Search",
-        "search_placeholder": "Search..."
-    },
-
-    "footer": {
-        "author": "Designed by Naicon @ Federal University of Ceará",
-        "rights": "All Rights Reserved"
-    }   
-};
+var language;
 
 // Function to get the language
 function getLanguage() {
     (localStorage.getItem('language') == null) ? setLanguage('en') : false;
-    $.ajax({ 
-        url:  '../Naicon/src/language/' +  localStorage.getItem('language') + '.json', 
-        dataType: 'json', async: false, dataType: 'json', 
-        success: function (lang) { 
-            language = lang 
-        } 
-    });
+    
+    if(localStorage.getItem('language') == 'pt'){
+        language = pt;
+    }else{
+        language = en;
+    }
 }
 
 // Function to set the language
 function setLanguage(lang) {
     localStorage.setItem('language', lang);
+    window.location.reload();
 }
 
 // Function to add the html code of header and footer
 function addHeaderFooter(){
-
+    console.log(language)
     // Getting elements
     header = document.getElementById('header');
     footer = document.getElementById('footer');
@@ -87,8 +60,8 @@ function addHeaderFooter(){
                     <button type="submit">${language.header.search}</button>
                 </li>
                 <li class="language">
-                    <a href="#" onclick="setLanguage('en')"><img src="./src/img/brazil-flag.png" alt="Brazil"></a>
-                    <a href="#" onclick="setLanguage('pt')"><img src="./src/img/usa-flag.png" alt="USA"></a>
+                    <a href="#" onclick="setLanguage('pt')"><img src="./src/img/brazil-flag.png" alt="Brazil"></a>
+                    <a href="#" onclick="setLanguage('en')"><img src="./src/img/usa-flag.png" alt="USA"></a>
                 </li>
             </ul>
         </nav>
